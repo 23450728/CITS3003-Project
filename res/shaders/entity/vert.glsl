7e32709d0083f0fdfde582d8b9574094c5c8ee60
yasaratano <yasapurwasamudra@gmail.com>
2024-05-15 14:57:32 +0800

    Did question E, and added  ws_view_dir lines to vertex shaders

--- res/shaders/entity/vert.glsl
+++ res/shaders/entity/vert.glsl
@@ -23,16 +23,20 @@ uniform vec3 diffuse_tint;
 uniform vec3 specular_tint;
 uniform vec3 ambient_tint;
 uniform float shininess;
+uniform float texture_scale;
 
 // Global data
 uniform vec3 ws_view_position;
 uniform mat4 projection_view_matrix;
 
+
+
 void main() {
     // Transform vertices
     vec3 ws_position = (model_matrix * vec4(vertex_position, 1.0f)).xyz;
     vec3 ws_normal = normalize(normal_matrix * normal);
-    vertex_out.texture_coordinate = texture_coordinate;
+    //change
+    vertex_out.texture_coordinate = texture_coordinate * (texture_scale);
     vertex_out.ws_position = ws_position;
     vertex_out.ws_normal = ws_normal;
 
